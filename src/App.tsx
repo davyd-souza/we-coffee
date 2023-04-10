42cec2a0f984774ed59fd531d37182eb59c988e3
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-10 00:50:57 -0300

    feat: create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+// COMPONENT
+import { Header } from './components/Header'
+
 export function App() {
   return (
     <div className="App">
+      <Header />
       <h1>We Coffee</h1>
     </div>
   )
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+// ASSETS
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import weCoffeeLogo from '../../assets/we-coffee.svg'
+
+export function Header() {
+  return (
+    <header className="py-4">
+      <div className="max-w-6xl mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <img className="w-12 aspect-square" src={weCoffeeLogo} alt="" />
+
+          <nav className="flex gap-2">
+            <a
+              className="p-2 border-b-2 border-b-transparent hocus:border-b-primary-400 hocus:text-primary-700 transition-color outline-none "
+              href="#"
+            >
+              Home
+            </a>
+            <a
+              className="p-2 border-b-2 border-b-transparent hocus:border-b-primary-400 hocus:text-primary-700 transition-color outline-none "
+              href="#"
+            >
+              Cardápio
+            </a>
+          </nav>
+
+          <div className="flex items-center justify-center gap-2">
+            <span className="p-2 flex  items-center justify-center gap-2 bg-primary-100 text-primary-700 font-bold">
+              <MapPin size={24} weight="fill" />
+              São Paulo, SP
+            </span>
+            <button className="p-2 rounded outline-none hocus:text-primary-600 focus-visible:ring ring-primary-400">
+              <ShoppingCart size={24} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
