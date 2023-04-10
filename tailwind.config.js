e043ee9c575c9e498e787f1b44b82366a4f14daf
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-10 00:48:07 -0300

    build: implement new variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.tsx', './index.html'],
@@ -15,11 +17,16 @@ export default {
           800: '#42392b',
         },
         secondary: {
+          200: '#b5b7d6',
           400: '#8487BA',
           600: '#484A6E',
         },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('hocus', ['&:hover', '&:focus-visible'])
+    }),
+  ],
 }
