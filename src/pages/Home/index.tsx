9cf18e95a9d24a8e62eb8cd0309b527d9ebfb95c
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-16 23:45:44 -0300

    feat: implement style for quality section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,13 @@
 import drinkBanner from '../../assets/drink-banner.png'
 import sweetBanner from '../../assets/sweet-banner.png'
 import separationBanner from '../../assets/separator-banner.webp'
+import churroImg from '../../assets/churro-img.webp'
 
 export function Home() {
   return (
     <main className="grid gap-20">
       <section className="max-w-6xl mx-auto px-6 grid gap-4 pt-20">
-        <h2 className="text-center font-light text-primary-700 text-sm tracking-wider">
+        <h2 className="text-center font-light text-primary-700 text-sm tracking-wider uppercase">
           Aqui você encontra os melhores produtos
         </h2>
 
@@ -62,18 +63,32 @@ export function Home() {
         </div>
       </section>
 
-      <section>
-        <img src="" alt="" />
-        <div>
-          <h2>Compromisso coma qualidade</h2>
-          <p>Fabricação própria com reposição diária</p>
-          <p>
-            Nossos processos são otimizados para garantir que todos os produtos
-            sejam entregues fresquinhos para você. Nossas bebidas são preparadas
-            na hora! Nossos pães e doces são produzidos e repostos diariamente.
-            Prezamos pela qualidade e excelência em cada detalhe.
-          </p>
-          <a href="#">Conheça</a>
+      <section className="max-w-6xl mx-auto px-6">
+        <h2 className="text-center font-light text-primary-700 text-sm tracking-wider uppercase">
+          Compromisso com a qualidade
+        </h2>
+        <div className="grid gap-4 grid-cols-2 items-center">
+          <img
+            src={churroImg}
+            alt="Two churros side by side with a label on top containing We Coffee"
+          />
+          <div className="grid gap-4">
+            <p className="text-3xl text-primary-700 font-bold">
+              Fabricação própria com reposição diária
+            </p>
+            <p className="text-primary-600">
+              Nossos processos são otimizados para garantir que todos os
+              produtos sejam entregues fresquinhos para você. Nossas bebidas são
+              preparadas na hora! Nossos pães e doces são produzidos e repostos
+              diariamente. Prezamos pela qualidade e excelência em cada detalhe.
+            </p>
+            <a
+              className="py-2 px-4 rounded-full text-white bg-primary-700 hocus:bg-primary-600 transition-colors outline-none focus-visible:ring ring-primary-600 ring-offset-2 ring-offset-white justify-self-start"
+              href="#"
+            >
+              Conheça
+            </a>
+          </div>
         </div>
       </section>
 
