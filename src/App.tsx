d8482d54d5aa1ae9251be186a2d6a2b455acbadc
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-05-01 19:00:19 -0300

    feat: create Cart context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+// COMPONENT
 import { Router } from './Router'
+
+// CONTEXT
+import { CartContextProvider } from 'contexts/CartContext'
 
 export function App() {
   return (
-    <>
+    <CartContextProvider>
       <Router />
-    </>
+    </CartContextProvider>
   )
 }
---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,43 @@
+// DEPENDENCY
+import { ReactNode, createContext, useState } from 'react'
+
+// TYPE
+type CartType = {
+  id: string
+  quantity: number
+}
+
+type CartContextType = {
+  cart: CartType[]
+  addItemToCart: (cartItem: CartType) => void
+}
+
+type CartContextProviderType = {
+  children: ReactNode
+}
+
+// CONTEXT
+export const CartContext = createContext({} as CartContextType)
+
+export function CartContextProvider({ children }: CartContextProviderType) {
+  const [cart, setCart] = useState<CartType[]>([])
+
+  const addItemToCart = (cartItem: CartType) => {
+    if (cartItem.quantity === 0) {
+      return
+    }
+
+    const newCartList = cart.filter((item) => item.id !== cartItem.id)
+    setCart([...newCartList, cartItem])
+  }
+
+  // useEffect(() => {
+  //   console.log('[CartContext > useEffect > cart]', cart)
+  // }, [cart])
+
+  return (
+    <CartContext.Provider value={{ cart, addItemToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
