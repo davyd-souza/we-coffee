674991ff97a0afd79afa8ddd8d3584271d071c7c
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-05-03 20:16:03 -0300

    refactor: remove cursor style from Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -12,7 +12,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
     <input
       {...props}
       ref={ref}
-      className="w-full p-2 border-2 border-primary-600 outline-none placeholder:text-neutral-400 [&:not(:disabled)]:hocus:border-primary-400 disabled:opacity-50 disabled:cursor-not-allowed transition-all peer/input"
+      className="w-full p-2 border-2 border-primary-600 outline-none placeholder:text-neutral-400 [&:not(:disabled)]:hocus:border-primary-400 disabled:opacity-50 transition-all peer/input"
     />
   )
 })
