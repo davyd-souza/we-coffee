d8ff3cc0d4382c381ed4dc8ef9cd82ba27e5bac4
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-05-04 01:50:06 -0300

    feat: implement quantity functions
    Added quantity increment, decrement and input change on cart context

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,5 +1,5 @@
 // DEPENDENCY
-import { ReactNode, createContext, useState } from 'react'
+import { ReactNode, createContext, useState, useEffect } from 'react'
 
 // TYPE
 type CartType = {
@@ -14,6 +14,9 @@ type CartContextType = {
   cart: CartType[]
   addItemToCart: (cartItem: CartType) => void
   removeCartItem: (cartItemId: string) => void
+  incrementQuantityCartItem: (cartItemId: string) => void
+  decrementQuantityCartItem: (cartItemId: string) => void
+  inputQuantityChangeCartItem: (cartItemId: string, value: number) => void
 }
 
 type CartContextProviderType = {
@@ -23,7 +26,32 @@ type CartContextProviderType = {
 // CONTEXT
 export const CartContext = createContext({} as CartContextType)
 
+// const CART_DATA = [
+//   {
+//     id: '1118d9c6-b426-48e2-862f-8551f6c8471f',
+//     name: 'Espresso',
+//     price: 6,
+//     image: 'http://localhost:5173/assets/images/cafe/quente/espresso.webp',
+//     quantity: 2,
+//   },
+//   {
+//     id: 'c72a8fa1-71a1-4c2a-b962-165750d3376c',
+//     name: 'Café Coado',
+//     price: 18,
+//     image: 'http://localhost:5173/assets/images/cafe/quente/coado.webp',
+//     quantity: 1,
+//   },
+//   {
+//     id: '89eff95c-5116-4891-81c2-7f0c1e71395d',
+//     name: 'Americano',
+//     price: 9,
+//     image: 'http://localhost:5173/assets/images/cafe/quente/americano.webp',
+//     quantity: 3,
+//   },
+// ]
+
 export function CartContextProvider({ children }: CartContextProviderType) {
+  // const [cart, setCart] = useState<CartType[]>(CART_DATA)
   const [cart, setCart] = useState<CartType[]>([])
 
   const addItemToCart = (cartItem: CartType) => {
@@ -40,12 +68,69 @@ export function CartContextProvider({ children }: CartContextProviderType) {
     setCart(newCartList)
   }
 
-  // useEffect(() => {
-  //   console.log('[CartContext > useEffect > cart]', cart)
-  // }, [cart])
+  const incrementQuantityCartItem = (cartItemId: string) => {
+    const newCartList = cart.map((item) => {
+      if (item.id === cartItemId) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        }
+      }
+      return item
+    })
+
+    setCart(newCartList)
+  }
+
+  const decrementQuantityCartItem = (cartItemId: string) => {
+    const newCartList = cart.map((item) => {
+      if (item.id === cartItemId) {
+        if (!(item.quantity <= 0)) {
+          return {
+            ...item,
+            quantity: item.quantity - 1,
+          }
+        }
+        return item
+      }
+      return item
+    })
+
+    setCart(newCartList)
+  }
+
+  const inputQuantityChangeCartItem = (cartItemId: string, value: number) => {
+    const newCartList = cart.map((item) => {
+      if (item.id === cartItemId) {
+        if (!Number.isNaN(value)) {
+          return {
+            ...item,
+            quantity: value,
+          }
+        }
+        return item
+      }
+      return item
+    })
+
+    setCart(newCartList)
+  }
+
+  useEffect(() => {
+    console.log('[CartContext > useEffect > cart]', cart)
+  }, [cart])
 
   return (
-    <CartContext.Provider value={{ cart, addItemToCart, removeCartItem }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addItemToCart,
+        removeCartItem,
+        incrementQuantityCartItem,
+        decrementQuantityCartItem,
+        inputQuantityChangeCartItem,
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
